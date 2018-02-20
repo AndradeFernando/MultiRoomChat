c8d5b09b7c8dafc4d6ec04fa2123240e5a5a2e50
ferandra <ferandra@ADMINIB-LR5RETV.br.ibm.com>
2018-02-20 15:24:35 -0300

    finalizando projeto multiroomchat

--- MultiRoomChat/app/controllers/chat.js
+++ MultiRoomChat/app/controllers/chat.js
@@ -1,14 +1,19 @@
 module.exports.iniciaChat = function(application, req, res){
+	
 	var dadosForm = req.body;
 	
 	req.assert('apelido','Nome/Apelido é obrigatório').notEmpty();
 	req.assert('apelido','Nome/Apelido deve ter entre 3 e 15 caracteres').len(3,15);
 	
 	var erros = req.validationErrors();
-if (erros){
-	//res.send('existem erros no formulário'+ erros)
-	res.render('index',{validacao:erros})
-	return
-}
-	res.render('chat2')
+	if (erros){
+		//res.send('existem erros no formulário'+ erros)
+		res.render('index',{validacao:erros})
+		return
+	}
+
+	
+	application.get('io').emit('msgParaCliente',{apelido: dadosForm.apelido, mensagem:' acabou de entrar no chat'});
+	
+	res.render('chat2',{dadosForm: dadosForm});
 }
